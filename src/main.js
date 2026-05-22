f3d3764455fec97780b9c2e4d49c99c830fa15b2
Antelmo Verdugo <antelmoverdugo@gmail.com>
2026-05-22 12:44:56 -0700

    /users/:id get & patch

--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ const app = express();
 const port = 3000;
 
 const swaggerDocument = YAML.load('./openapi.yaml');
+const users = new Map([
+  [1, { id: 1, name: 'Jane Doe', age: 30, email: 'jane@example.com' }],
+]);
+let nextUserId = 2;
 
 app.use(express.json());
 
@@ -15,21 +19,14 @@ app.use(
     apiSpec: swaggerDocument,
     validateRequests: true,
     validateResponses: true,
-    ignorePaths: /.*\/docs.*/
+    ignorePaths: /.*\/docs.*/,
   })
 );
 
-app.use((err, req, res, next) => {
-  res.status(err.status || 500).json({
-    message: err.message,
-    errors: err.errors,
-  });
-});
-
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
-app.use('/', (req, res) => {
-  res.send('API First course');
+app.get('/', (req, res) => {
+  res.json('API First course');
 });
 
 app.get('/hello', (req, res) => {
@@ -37,11 +34,56 @@ app.get('/hello', (req, res) => {
 });
 
 app.post('/users', (req, res) => {
-  const body = req.body;
+  const user = {
+    id: nextUserId,
+    ...req.body,
+  };
+
+  users.set(nextUserId, user);
+  nextUserId += 1;
 
   res.status(201).json({
-    id: 1,
-    ...body,
+    ...user,
+  });
+});
+
+app.get('/users/:id', (req, res) => {
+  const id = Number.parseInt(req.params.id, 10);
+  const user = users.get(id);
+
+  if (!user) {
+    return res.status(404).json({
+      message: 'User not found',
+    });
+  }
+
+  return res.json(user);
+});
+
+app.patch('/users/:id', (req, res) => {
+  const id = Number.parseInt(req.params.id, 10);
+  const user = users.get(id);
+
+  if (!user) {
+    return res.status(404).json({
+      message: 'User not found',
+    });
+  }
+
+  const updatedUser = {
+    ...user,
+    name: req.body.name,
+  };
+
+  users.set(id, updatedUser);
+
+  return res.json(updatedUser);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors,
   });
 });
 
