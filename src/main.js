e7848c6306ec2410f9e6ef03c526dd530731ae8f
Antelmo Verdugo <antelmoverdugo@gmail.com>
2026-05-22 11:52:01 -0700

    openapi validator

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import swaggerUI from 'swagger-ui-express';
 import YAML from 'yamljs';
+import OpenApiValidator from 'express-openapi-validator';
 
 const app = express();
 const port = 3000;
@@ -8,6 +9,26 @@ const port = 3000;
 const swaggerDocument = YAML.load('./openapi.yaml');
 
 app.use(express.json());
+
+app.use(
+  OpenApiValidator.middleware({
+    apiSpec: swaggerDocument,
+    validateRequests: true,
+    validateResponses: true,
+    ignorePaths: /.*\/docs.*/
+  })
+);
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors,
+  });
+});
+
+app.use('/', (req, res) => {
+  res.send('API First course');
+});
 
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
